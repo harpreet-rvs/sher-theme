aaf0383b3c88cbc5c2e519a5a134ef670dfd3ed6
hprvsmedia <47854255+hprvsmedia@users.noreply.github.com>
2019-03-12 17:19:06 +0530

    update

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sharwood/Wood',
+    'frontend/hprvsmedia/sher-theme',
     __DIR__
 );
